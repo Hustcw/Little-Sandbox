f6bcafa5190e9b19872a6dcb621c9d0d10d5438d
Hao Wang <hustcw1998@gmail.com>
2019-11-13 22:15:40 +0900

    fix name collision

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='little-sandbox-py',
-    version='1.0.2',
+    version='1.0.3',
     description='A sandbox for running python scripts',
     url='https://github.com/Hustcw/Little-Sandbox',
     author='hustcw',
